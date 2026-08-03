6526a9d45b040611b3838b928723866346267f24
Paulo Leite <phvleite@gmail.com>
2026-08-03 10:51:07 -0300

    task-002.15: adicionar conteudo explicativo sobre o portal e o funcionamento do servico

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -27,7 +27,7 @@ export default async function DashboardLayout({
                 role={session.user.role}
                 sectorsOk={sectorStatus.valid}
             />
-            <div className="lg:pl-65">
+            <div className="lg:pl-72">
                 <div className="px-4 py-6 sm:px-6 sm:py-8">{children}</div>
             </div>
         </div>
